5756f27415b7ea077abd9da2f680b6b98a41ad66
i2x <0x8084@gmail.com>
2024-12-12 15:23:11 +0700

    update

--- notes/urls.py
+++ notes/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('create/', views.create, name='create'),
     path('<int:note_id>/update/', views.update, name='update'),
     path('<int:note_id>/delete/', views.delete, name='delete'),
+    path('datetime/', views.show_datetime, name='show_datetime'),
+    path('directory/', views.show_directory_listing, name='show_directory_listing'),
+
+
 ]
