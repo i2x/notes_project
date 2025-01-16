a96a2be07ce1ca7e63f1ecb860e385a00c0cdc90
i2x <0x8084@gmail.com>
2025-01-16 09:07:54 +0700

    change regex example

--- regex/urls.py
+++ regex/urls.py
@@ -1,10 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
-from django.urls import re_path
-
 
 urlpatterns = [
     path('', views.index, name='regex_index'),
     re_path(r'^articles/(?P<year>[0-9]{4})/$', views.article_by_year, name='article_by_year'),
-    re_path(r'^profile/(?P<username>[a-zA-Z0-9_]+)/$', views.profile, name='profile'),
+    re_path(r'^check-email/(?P<email>.+)/$', views.check_email, name='check_email'),
 ]
--- regex/views.py
+++ regex/views.py
@@ -1,11 +1,18 @@
 from django.http import HttpResponse
+import re
 
 def index(request):
     return HttpResponse("Welcome to the Regex App!")
 
-
 def article_by_year(request, year):
     return HttpResponse(f"Articles from the year {year}")
 
-def profile(request, username):
-    return HttpResponse(f"Profile page of user: {username}")
+def check_email(request, email):
+    # Updated Regex pattern for email validation
+    pattern = r'^[\w\-\.]+@([\w\-]+\.)+[\w\-]{2,4}$'
+    
+    # Validate the email with the regex
+    if re.fullmatch(pattern, email):
+        return HttpResponse(f"The email '{email}' is valid and matches the correct email format.")
+    else:
+        return HttpResponse(f"The email '{email}' is invalid or does not match the correct email format.")
